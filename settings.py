8a6d6fca68cf2a6d69fe7f38783ed730c3ae33db
Maurice Ilagan <maurice.ilagan@checkpt.com>
2019-09-04 20:12:09 -0400

    Updates to support postgresql

--- settings.py
+++ settings.py
@@ -2,6 +2,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:mysecretpassword@localhost/books'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'secret'
